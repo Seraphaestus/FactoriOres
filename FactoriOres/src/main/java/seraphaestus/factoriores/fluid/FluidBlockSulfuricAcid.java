41d8d7c5edb40d1ab7426697703ddd33b49b149f
seraphaestus <seraphaestus@gmail.com>
2021-06-08 08:15:03 +0100

    Acid now damages mobs

--- FactoriOres/src/main/java/seraphaestus/factoriores/fluid/FluidBlockSulfuricAcid.java
+++ FactoriOres/src/main/java/seraphaestus/factoriores/fluid/FluidBlockSulfuricAcid.java
@@ -5,7 +5,12 @@ import java.util.function.Supplier;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.FlowingFluidBlock;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.boss.WitherEntity;
+import net.minecraft.entity.item.ArmorStandEntity;
+import net.minecraft.entity.monster.AbstractSkeletonEntity;
+import net.minecraft.entity.monster.SlimeEntity;
+import net.minecraft.entity.passive.IronGolemEntity;
 import net.minecraft.fluid.FlowingFluid;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.Direction;
@@ -21,9 +26,19 @@ public class FluidBlockSulfuricAcid extends FlowingFluidBlock {
 	
 	@Override
     public void onEntityCollision(BlockState state, World worldIn, BlockPos pos, Entity entityIn) {
-        if (entityIn instanceof PlayerEntity) {
-            PlayerEntity player = (PlayerEntity) entityIn;
-            player.attackEntityFrom(DamageSource.LAVA, 1.0f);
+        if (entityIn instanceof LivingEntity) {
+        	if (entityIn instanceof ArmorStandEntity ||
+        		entityIn instanceof AbstractSkeletonEntity ||
+        		entityIn instanceof WitherEntity) return;
+        	
+        	LivingEntity entity = (LivingEntity) entityIn;
+        	
+        	float damage = 3.0f;
+        	if (entityIn instanceof SlimeEntity) damage *= 2;
+        	else if (entityIn instanceof IronGolemEntity) damage /= 2;
+        	
+        	entity.attackEntityFrom(DamageSource.LAVA, damage);
+            return;
         }
     }
 	
